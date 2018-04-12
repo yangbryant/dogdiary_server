780538c758684af276d0d4358fa5068260425258
liyang <yang.li@zxthealth.com>
2018-04-12 18:05:55 +0800

    config: 修改路由

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,13 +34,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const error = req.app.get('env') === 'development' ? { 'code' : err.status, 'message' : err.message } : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(error);
 });
 
 // start server
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ Welcome: '狗狗日记' });
 });
 
 module.exports = router;
